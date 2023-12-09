ada2043d1940b336879847b9c53f3ff7e0ecb6a9
SK SHALU BASHA <shalubasha012@gmail.com>
2023-12-09 18:18:49 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+    form.submit();
+});
+
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+      $.ajax({
+        url: 'php/login.php', 
+        type: 'POST', 
+        data: $('form').serialize(), 
+        success: function(response) {
+          alert(response); 
+        }
+      });
+    });
+  });
